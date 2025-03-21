9f11948f73a43c0d31561b4e9ef5c0ac130c6f5b
ayesha-afaq <bla4753ayesha@gmail.com>
2025-03-21 20:12:40 +0500

    Update sheer.py

--- sheer.py
+++ sheer.py
@@ -1,10 +1,30 @@
-# import csv
-# with open('Book1.csv', newline='') as csvfile:
-#     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-#     for row in spamreader:
-#         print(', '.join(row))
+import tkinter as tk
+from tkinter import messagebox, simpledialog, ttk
+from tkinter import Label,Button,Frame,Radiobutton
 
-print('hello')
-print('hiii')
 
-print('3333')
+
+class Rental_System:
+   def __init__(self,root):
+      self.root=root
+      self.root.title("CEP")
+      root.geometry("500x500+50+50")
+      root.minsize(200, 100)
+      root.maxsize(800, 800)
+      
+      header = Label(self.root,text="|||~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~CAR RENTAL SYSTEM~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|||")
+      header.pack()#Displays a header.
+      
+      self.menu_frame = tk.Frame(root)
+      self.menu_frame.pack(pady=50)
+      tk.Button(self.menu_frame, text="USER", width=30,height=5,bg='aqua').pack(pady=30)
+      tk.Button(self.menu_frame, text="ADMINISTRATOR", width=30,height=5,bg='aqua').pack(pady=30)
+
+
+
+
+
+if __name__ == "__main__":
+    root=tk.Tk()#Creates the main tkinter window (root).
+    app=Rental_System(root)#Initializes the Rental System class.
+    root.mainloop()#Starts the tkinter event loop (root.mainloop()).
