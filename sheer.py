0253a98756f5b283626fbadf70dbbeba3d5dc23f
ayesha-afaq <bla4753ayesha@gmail.com>
2025-04-17 12:54:57 +0500

    admin

--- sheer.py
+++ sheer.py
@@ -1,5 +1,6 @@
+import os
 import customtkinter as ctk
-from customtkinter import CTkLabel,CTkButton,CTkEntry,CTkFrame
+from customtkinter import CTkLabel,CTkButton,CTkEntry,CTkFrame,CTkInputDialog
 ctk.set_appearance_mode('dark')
 ctk.set_default_color_theme('green')
 
@@ -28,23 +29,38 @@ class Admin(Account):
    pass
 
 class Rental_System:
-  def __init__(self,root):
-      self.root=root
-      self.root.title('CAR RENTAL SYSTEM')
-      self.root.geometry('500x500')
-      header=CTkLabel(
-         self.root,
-         width=10,
-         corner_radius=10,
-         text='Welcome To The Car Rental System\n\nPlease Choose Your Account Type',
-         font=("Arial", 16)).pack(pady=20)
+   
+   def __init__(self,root):
+         self.root=root
+         self.root.title('CAR RENTAL SYSTEM')
+         self.root.geometry('500x500')
+         header=CTkLabel(
+            self.root,
+            width=10,
+            corner_radius=10,
+            text='Welcome To The Car Rental System\n\nPlease Choose Your Account Type',
+            font=("Arial", 16)).pack(pady=20)
 
-    
-      
-      self.menu_frame =CTkFrame(root, width=500, height=500)
-      self.menu_frame.pack(pady=40)
-      CTkButton(master=self.menu_frame,text='USER ACCOUNT',corner_radius=10,fg_color='blue').pack(pady=10)
-      CTkButton(master=self.menu_frame,text='ADMINISTRATOR',corner_radius=10,fg_color='blue').pack(pady=10)
+         
+         
+         self.menu_frame =CTkFrame(root, width=500, height=500)
+         self.menu_frame.pack(pady=40)
+         CTkButton(master=self.menu_frame,text='USER ACCOUNT',corner_radius=10,fg_color='blue').pack(pady=10)
+         CTkButton(master=self.menu_frame,text='ADMINISTRATOR',command=self.admin_work,corner_radius=10,fg_color='blue').pack(pady=10)
+
+   def admin_work(self):
+         if not os.path.isfile('adminpass.txt'): ## checks if given path ki file exist krti hae
+            pass ## creates admin object
+         else: 
+            print('file exists')
+            with open('adminpass.txt') as pass_file:
+               p=pass_file.read().strip()
+            
+            password=CTkInputDialog(text='Enter Password',title='Admin Login').get_input()
+         
+            if p==password:
+               print('good')
+            else: print('not gud')
       
 
 
