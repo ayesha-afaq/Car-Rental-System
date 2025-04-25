bf3ae9a34f80d11bf2f2507e77cac1f9dd2f5a9c
ayesha-afaq <bla4753ayesha@gmail.com>
2025-04-25 21:04:25 +0500

    trying to correct smthingg

--- test.py
+++ test.py
@@ -5,15 +5,33 @@ from customtkinter import CTkLabel,CTkButton,CTkEntry,CTkFrame,CTkInputDialog,CT
 
 ctk.set_appearance_mode('dark')
 ctk.set_default_color_theme('green')
-def func():
-    print(entry.get())
-    print(entry)
-
 root=ctk.CTk()
-entry=CTkEntry(root,placeholder_text='Enter your name')
-entry.place(relx=0.5,rely=0.4,anchor='center')
-entrybutton=CTkButton(root,text='Submit',command=func)
-entrybutton.place(relx=0.5,rely=0.5,anchor='center')
+
+def func():
+   root.withdraw()
+   print('hello')
+   print('hello')
+   print('hello')
+   print('window coming back')
+   
+   root.deiconify()
+   print('window came')
+
+CTkButton(root,text='window1\npress',command=func).pack()
+
+
+
+
+# def func():
+#     print(entry.get())
+#     print(entry)
+
+# root=ctk.CTk()
+# entry=CTkEntry(root,placeholder_text='Enter your name')
+# entry.place(relx=0.5,rely=0.4,anchor='center')
+# entrybutton=CTkButton(root,text='Submit',command=func)
+# entrybutton.place(relx=0.5,rely=0.5,anchor='center')
 
 
 root.mainloop()
+
